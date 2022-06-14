41021406eb727d85b57a3f8760e9af9908023ea1
Martin Ha <martindn.ha@gmail.com>
2022-06-14 08:19:42 +0000

    Change redirect URL back to Portfolio homepage

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -20,5 +20,7 @@ public class FormHandlerServlet extends HttpServlet {
 
     // Write the value to the response so the user can see it.
     response.getWriter().println("You submitted: " + textValue);
+    //Redirect client to front page
+    //response.sendRedirect("https://mha-sps-summer22.wl.r.appspot.com");
   }
 }
