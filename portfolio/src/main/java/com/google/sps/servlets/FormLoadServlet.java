72c50af1ad27009e8373c57e6cf42389198bfad7
Martin Ha <martindn.ha@gmail.com>
2022-06-20 03:38:46 +0000

    Creation of Servlet to load datastore, has bugs and does not work

---
+++ portfolio/src/main/java/com/google/sps/servlets/FormLoadServlet.java
@@ -0,0 +1,42 @@
+package com.google.sps.servlets;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.StructuredQuery.OrderBy;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet responsible for listing tasks. */
+@WebServlet("/get-inputs")
+public class FormLoadServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    Query<Entity> query =
+        Query.newEntityQueryBuilder().setKind("Task").build();
+    QueryResults<Entity> results = datastore.run(query);
+
+while (results.hasNext()) {
+  Entity entity = results.next();
+
+  long id = entity.getKey().getId();
+  String comment = entity.getString("input");
+
+}
+
+    Gson gson = new Gson();
+
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(Task));
+  }
+}
